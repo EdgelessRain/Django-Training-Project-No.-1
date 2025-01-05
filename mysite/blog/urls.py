9cb30dd3d6257fe2271fb1bb76524148eacbc571
EdgelessRain <oseviz31@mail.ru>
2025-01-05 21:59:14 +0300

    Adding URL templates for views
    A new URL pattern defined using the include function references the URL patterns defined in the blog application so they are included under the blog/ path. These patterns are inserted within the blog namespace.

---
+++ mysite/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+#The namespace is defined by the application using the app_name variable.
+app_name = 'blog'
+
+urlspaterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+]
